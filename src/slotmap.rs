c324d2cf5337c39513102ea44caa389764da77a8
memoryleak47 <memoryleak47@protonmail.ch>
2026-04-01 17:18:32 +0200

    ~slotmap

--- src/slotmap.rs
+++ src/slotmap.rs
@@ -11,8 +11,22 @@ impl SlotMap {
         SlotMap(slots.iter().map(|x| (*x, *x)).collect())
     }
 
-    pub fn compose(&self, other: &SlotMap) -> SlotMap {
-        todo!()
+    pub fn mk(iter: impl Iterator<Item=(Slot, Slot)>) -> SlotMap {
+        let mut v: Vec<_> = iter.collect();
+        v.sort_by_key(|(x, _)| *x);
+
+        SlotMap(v)
+    }
+
+    // m1*m2[x] = m1[m2[x]]
+    pub fn compose(&self, m2: &SlotMap) -> SlotMap {
+        let m1 = self;
+
+        SlotMap::mk(m2.0.iter().map(|(k, v)| (*k, m1[*v])))
+    }
+
+    pub fn inverse(&self) -> SlotMap {
+        SlotMap::mk(self.0.iter().map(|(x, y)| (*y, *x)))
     }
 }
 
@@ -20,6 +34,7 @@ impl Index<Slot> for SlotMap {
     type Output = Slot;
 
     fn index(&self, i: Slot) -> &Self::Output {
+        // TODO binary search.
         &self.0.iter().find(|(x, y)| *x == i).unwrap().1
     }
 }
