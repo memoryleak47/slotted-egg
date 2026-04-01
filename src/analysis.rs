83eaefaa1433467c3db5a33b730612f1550813f1
memoryleak47 <memoryleak47@protonmail.ch>
2026-04-01 14:43:22 +0200

    ...

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,14 +1,64 @@
 use crate::*;
 
 struct LambdaAnalysis;
-#[derive(Debug)]
-struct LambdaData;
+
+#[derive(Debug, Clone)]
+struct LambdaData {
+    slots: BTreeSet<Slot>,
+    leader: (SlotMap, Id),
+    group: BTreeSet<SlotMap>,
+}
 
 impl Analysis<Lambda> for LambdaAnalysis {
     type Data = LambdaData;
-    fn make(eg: &mut EGraph<Lambda, Self>, n: &Lambda, x: Id) -> Self::Data { todo!() }
-    fn merge(&mut self, a: &mut LambdaData, b: LambdaData) -> DidMerge { todo!() }
+
+    fn make(eg: &mut EGraph<Lambda, Self>, n: &Lambda, x: Id) -> Self::Data {
+        let slots = match n {
+            Lambda::Lam(s, b) => eg[*b].data.slots.iter().copied().filter(|x| x != s).collect(),
+            Lambda::App([a, b]) => &eg[*a].data.slots | &eg[*b].data.slots,
+            Lambda::Var(s) => std::iter::once(*s).collect(),
+            Lambda::Rename(m, a) => eg[*a].data.slots.iter().map(|x| m[*x]).collect(),
+        };
+        let identity = SlotMap::identity(&slots);
+        LambdaData {
+            slots,
+            leader: (identity.clone(), x),
+            group: std::iter::once(identity).collect(),
+        }
+    }
+
+    fn merge(&mut self, a: &mut LambdaData, b: LambdaData) -> DidMerge {
+        let (ma, la) = &a.leader;
+        let (mb, lb) = &b.leader;
+        if la == lb { // symmetries
+            let l = la;
+            // ma * l = mb * l
+            // l = ma⁻¹ * mb * l
+
+            // TODO how to do that? I have no e-graph access.
+            // let mut data = [l].data.clone();
+            // data.group.push(ma.inverse().compose(mb));
+            // complete_group(&mut data.group);
+            // egraph.set_analysis_data(l, data);
+        } else {
+            todo!()
+        }
+    }
 }
 
+fn complete_group(group: &mut BTreeSet<SlotMap>) {
+    loop {
+        let n = group.len();
 
+        let mut extra = BTreeSet::new();
 
+        for x in group {
+            for y in group {
+                extra.push(x.compose(y));
+            }
+        }
+        group.extend(extra);
+
+        if n == group.len() { break }
+    }
+}
--- src/slotmap.rs
+++ src/slotmap.rs
@@ -1,4 +1,21 @@
+use std::ops::Index;
+pub use std::collections::BTreeSet;
+
 pub type Slot = usize;
 
 #[derive(Debug, PartialEq, Eq, Ord, PartialOrd, Hash, Clone)]
 pub struct SlotMap(Vec<(Slot, Slot)>);
+
+impl SlotMap {
+    pub fn identity(slots: &BTreeSet<Slot>) -> Self {
+        SlotMap(slots.iter().map(|x| (*x, *x)).collect())
+    }
+}
+
+impl Index<Slot> for SlotMap {
+    type Output = Slot;
+
+    fn index(&self, i: Slot) -> &Self::Output {
+        &self.0.iter().find(|(x, y)| *x == i).unwrap().1
+    }
+}
